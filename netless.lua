172202ee35840c1f57e545a69fa330e4f78b07d0
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-05-01 08:18:03 -0700

    added hats support and modified character fling

--- netless.lua
+++ netless.lua
@@ -19,12 +19,13 @@ if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 	for _, connection in ipairs(_G.Connections) do
 		connection:Disconnect()
 	end
-	
+
 	_G.Settings = {
 		WaitTime = _G.Settings.WaitTime or 5,
-		DisableAnimation = _G.Settings.DisableAnimation or false,
-		PlayerCanCollide = _G.Settings.PlayerCanCollide or true,
-		CanFlingPlayers = _G.Settings.CanFlingPlayers or false
+		DisableAnimations = _G.Settings.DisableAnimations or false,
+		PlayerCanCollide = _G.Settings.PlayerCanCollide or false,
+		RemoveAccessories = _G.Settings.RemoveAccessories or false,
+		EnableCharacterFling = _G.Settings.EnableCharacterFling or false,
 	}
 
 	HRP = Character.HumanoidRootPart
@@ -51,38 +52,31 @@ if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 		end
 		WaitTime = 5
 	end
-	
+
 	local Folder = Instance.new("Folder")
 	Folder.Name = "NETLESS-REANIMATE"
 	local DummyChar = game:GetObjects("rbxassetid://5904819435")[1]
 	DummyChar.Name = "Dummy"
 	DummyChar.Parent = Folder
-	
+
 	local AntiSpawnChar = Instance.new("Model")
 	local FakeHumanoid = Instance.new("Humanoid")
 	FakeHumanoid.Name = "Humanoid"
 	FakeHumanoid.Parent = AntiSpawnChar
 	AntiSpawnChar.Parent = Folder
-	
+
 	local RJointAtt = Instance.new("Attachment")
 	RJointAtt.Name = "RootJoint"
 	RJointAtt.CFrame = DummyChar.HumanoidRootPart.RootJoint.C0
-	
-	local AngularVel = Instance.new("BodyAngularVelocity")
-	AngularVel.MaxTorque = Vector3.new(math.huge, math.huge, math.huge)
-	AngularVel.AngularVelocity = Vector3.new(2500, 2500, 2500)
-	
+
 	Character.Animate.Disabled = true
 	Humanoid.Animator:Destroy()
-	HRP.Anchored = true
 	Player.Character = AntiSpawnChar
 	wait(WaitTime)
 	Player.Character = Character
 	wait(_G.Settings.WaitTime)
 	Character:BreakJoints()
-	HRP.Anchored = false
 	RJointAtt.Parent = HRP
-	AngularVel.Parent = HRP
 
 	Folder.Parent = Character
 	DummyChar:SetPrimaryPartCFrame(OldPos)
@@ -104,6 +98,17 @@ if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 			local Attachment = Instance.new("Attachment")
 			Attachment.Name = "Joint"
 			Attachment.Parent = object
+		elseif object:IsA("Accessory") then
+			if not _G.Settings.RemoveAccessories then
+				local Clone = object:Clone()
+				Clone.Handle.Transparency = 1
+				Clone.Parent = DummyChar
+
+				local Attachment = Instance.new("Attachment")
+				Attachment.Parent = object.Handle
+			else
+				object:Destroy()
+			end
 		end
 	end
 
@@ -116,7 +121,7 @@ if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 	_G.Connections[1] = RunService.Heartbeat:Connect(function()
         DummyChar.Humanoid:Move(Humanoid.MoveDirection)
 
-		for _, object in ipairs(Character:GetChildren()) do
+		for _, object in ipairs(Character:GetDescendants()) do
 			if object:IsA("BasePart") then
 				object.LocalTransparencyModifier = DummyChar.Head.LocalTransparencyModifier
 			end
@@ -130,15 +135,15 @@ if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 			DummyChar.Humanoid.Jump = true
 		end
 	end)
-	
+
 	_G.Connections[2] = RunService.Stepped:Connect(function()
-		if _G.Settings.PlayerCanCollide then
+		if _G.Settings.PlayerCanCollide == true then
 			for _, object in ipairs(Character:GetChildren()) do
 				if object:IsA("BasePart") and object.CanCollide == true then
 					object.CanCollide = false
 				end
 			end
-		else
+		elseif _G.Settings.PlayerCanCollide == false then
 			for _, object in ipairs(Character:GetDescendants()) do
 				if object:IsA("BasePart") and object.CanCollide == true then
 					object.CanCollide = false
@@ -152,24 +157,33 @@ if Humanoid.RigType == Enum.HumanoidRigType.R6 then
 			if object:IsA("BasePart") then
 				if object.Name == "HumanoidRootPart" then
 					object.CFrame = CFrame.new(object.Joint.Position)
+					object.Velocity = Vector3.new(10e8, 10e8, 10e8)
 				else
 					object.CFrame = DummyChar[object.Name].CFrame * object.Joint.CFrame
-					if not _G.Settings.CanFlingPlayers then object.Velocity = Vector3.new(50, 50, 50) end
+					if not _G.Settings.EnableCharacterFling then
+						object.Velocity = Vector3.new(50, 50, 50)
+					else
+						object.Velocity = Vector3.new(-10e8, -10e8, -10e8)
+					end
 				end
+			elseif object:IsA("Accessory") then
+				object.Handle.CFrame = DummyChar[object.Name].Handle.CFrame * object.Handle.Attachment.CFrame
+				object.Handle.Velocity = Vector3.new(50, 50, 50)
 			end
 		end
 	end)
 
 	local ResetBindable = Instance.new("BindableEvent")
-	_G.Connections[4] = ResetBindable.Event:Connect(function()
-		if not game:GetService("Players").LocalPlayer.Character:FindFirstChild("NETLESS-REANIMATE") then
-		    game:GetService("Players").LocalPlayer.Character:BreakJoints()
+	if _G.PlayerResetConnection then _G.PlayerResetConnection:Disconnect() end
+	_G.PlayerResetConnection = ResetBindable.Event:Connect(function()
+		if not Player.Character:FindFirstChild("NETLESS-REANIMATE") then
+			Player.Character:BreakJoints()
 		else
-            Character:Destroy()
-            for _, connection in ipairs(_G.Connections) do
-                connection:Disconnect()
-            end
-            _G.Connections = {}
+			Character:Destroy()
+			for _, connection in ipairs(_G.Connections) do
+				connection:Disconnect()
+			end
+			_G.Connections = {}
 		end
 	end)
 	StarterGui:SetCore("ResetButtonCallback", ResetBindable)
