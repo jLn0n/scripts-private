7ad8b7de5e937a50113cf3221c6c01380530890a
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-06-28 07:21:26 -0700

    Update blackhole.lua

--- blackhole.lua
+++ blackhole.lua
@@ -13,17 +13,18 @@ local BlackholePos = Mouse.Hit.Position + Vector3.new(0, 2.5, 0)
 -- // MAIN
 AttHolder.Anchored, AttHolder.CanCollide, AttHolder.Transparency = true, false, .5
 local ForcePart = function(object)
-	if object:IsA("BasePart") and (object.Anchored and object:IsDescendantOf(Workspace.Structure) or not object.Anchored) and not object:IsDescendantOf(Player.Character) then
+	if object:IsA("BasePart") and ((game.PlaceId == 189707 and object.Anchored and object:IsDescendantOf(Workspace.Structure)) or not object.Anchored) and not object:IsDescendantOf(Player.Character) then
 		for _, sobject in ipairs(object:GetChildren()) do
 			if sobject:IsA("Attachment") and sobject:IsA("AlignPosition") and sobject:IsA("Torque") and sobject:IsA("BodyMover") or sobject:IsA("RocketPropulsion") then
 				sobject:Destroy()
 			end
 		end
+        object.CanCollide = false
 		local Torque, APos, Attachment0 = Instance.new("Torque", object), Instance.new("AlignPosition", object), Instance.new("Attachment", object)
 		Torque.Attachment0 = Attachment0
-		Torque.Torque = Vector3.new(10e10, 10e10, 10e10)
+		Torque.Torque = Vector3.new(1e10, 1e10, 1e10)
 		APos.Attachment0, APos.Attachment1 = Attachment0, Attachment1
-		APos.MaxForce = 10e10
+		APos.MaxForce = 9999999999
 		APos.MaxVelocity = math.huge
 		APos.Responsiveness = 200
 	end
@@ -47,5 +48,6 @@ RunService.RenderStepped:Connect(function()
 	settings().Physics.AllowSleep = false
 	settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Disabled
 	setsimulationradius(1e10, 1e10)
+    Player.ReplicationFocus = Workspace
 	AttHolder.Position = BlackholePos
 end)
