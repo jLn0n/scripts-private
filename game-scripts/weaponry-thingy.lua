9e89f790a1609c5bc105314fcb266bc2e05adcc2
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-10-06 16:54:35 +0800

    damn new changes
    * epic settings
        * added aimPart for semi aim
        * added fireCount for multiple bullets

--- game-scripts/weaponry-thingy.lua
+++ game-scripts/weaponry-thingy.lua
@@ -1,3 +1,6 @@
+-- settings
+local aimPart = "head" -- [HRP, HEAD] (can be cAsE sENsItIvE)
+local fireCount = 7 -- multiplies bullets
 -- services
 local guiService = game:GetService("GuiService")
 local players = game:GetService("Players")
@@ -8,7 +11,6 @@ local camera = workspace.CurrentCamera
 local player = players.LocalPlayer
 local mouse = player:GetMouse()
 -- modules
-local clientFwUpvals
 local rayCastClient, recoilHandler = require(repStorage.ClientModules.RayCastClient), require(repStorage.ClientModules.RecoilCamHandler)
 -- functions
 local function getClientFramework()
@@ -19,22 +21,22 @@ local function getClientFramework()
 		end
 	end
 end
+local function checkPlr(plr)
+	return plr == player and (plr.TeamColor == player.TeamColor) and not (plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") and not plr.Character:FindFirstChildWhichIsA("ForceField"))
+end
 local function getNearestPlrByCursor()
 	local nearPlrs = table.create(0)
 	for _, plr in ipairs(players:GetPlayers()) do
-		if plr == player and (plr.TeamColor == player.TeamColor) and not (plr.Character and plr.Character:FindFirstChild("Head")) then continue end
 		local p_char = plr.Character
-		if p_char then
-			local p_head = p_char:FindFirstChild("Head")
-			local posVec3, isVisible = camera:WorldToScreenPoint(p_head.Position)
-			if isVisible then
-				local mouseVec2, posVec2 = Vector2.new(mouse.X, mouse.Y), Vector2.new(posVec3.X, posVec3.Y)
-				local distance = (mouseVec2 - posVec2).Magnitude
-				table.insert(nearPlrs, {
-					plr = plr,
-					dist = distance
-				})
-			end
+		if not checkPlr(plr) and p_char then
+			local p_dPart = p_char:FindFirstChild("HumanoidRootPart")
+			local posVec3, _unused = camera:WorldToScreenPoint(p_dPart.Position)
+			local mouseVec2, posVec2 = Vector2.new(mouse.X, mouse.Y), Vector2.new(posVec3.X, posVec3.Y)
+			local distance = (mouseVec2 - posVec2).Magnitude
+			table.insert(nearPlrs, {
+				plr = plr,
+				dist = distance
+			})
 		end
 	end
 	table.sort(nearPlrs, function(x, y)
@@ -42,24 +44,30 @@ local function getNearestPlrByCursor()
 	end)
 	return (nearPlrs and #nearPlrs ~= 0) and nearPlrs[1].plr or nil
 end
+-- variables
+local clientFwUpvals = getsenv(getClientFramework()).CheckIsToolValid
 -- main
-clientFwUpvals = getsenv(getClientFramework()).CheckIsToolValid
 runService.RenderStepped:Connect(function()
-	pcall(function() -- inf ammo and no spread
-		for weapon = 1, 2 do
-			local weaponData = debug.getupvalue(clientFwUpvals, 1)[weapon]
-			weaponData.CurrentAmmo = weaponData.WeaponStats.MaxAmmo
-			weaponData.CurrentAccuracy = 0
+	local weaponsData = debug.getupvalue(clientFwUpvals, 1)
+	for _, weaponData in pairs(weaponsData) do
+		if weaponData.FriendlyName ~= "Knife" then
+			weaponData.CurrentAmmo = weaponData.WeaponStats.MaxAmmo -- infinite ammo
+			weaponData.CurrentAccuracy = 0 -- no spread
+			weaponData.WeaponStats.FireMode = {
+				["Name"] = "Auto", -- always auto
+				["Round"] = fireCount or 1 -- modifiable bullets
+			}
 		end
-	end)
+	end
 end)
 local oldCastMouse = rayCastClient.CastRayMouse
 rayCastClient.CastRayMouse = function(_camera, x, y) -- silent aim
 	local nearestPlr = getNearestPlrByCursor()
 	if nearestPlr then
+		aimPart = string.lower(aimPart)
 		local p_char = nearestPlr.Character
-		local p_head = p_char:FindFirstChild("Head")
-		local newVec2 = camera:WorldToScreenPoint(p_head.Position)
+		local p_targetPart = p_char:FindFirstChild(aimPart == "hrp" and "HumanoidRootPart" or aimPart == "head" and "Head" or nil)
+		local newVec2 = camera:WorldToScreenPoint(p_targetPart.Position)
 		x, y = newVec2.X, newVec2.Y + guiService:GetGuiInset().Y
 	end
 	return oldCastMouse(_camera, x, y)
