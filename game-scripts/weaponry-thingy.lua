ddb0af999c83768938514dc9ac228548a5e8316c
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-10-07 16:05:40 +0800

    wow !!!1!!!1!!! ui library
    yeah boi

--- game-scripts/weaponry-thingy.lua
+++ game-scripts/weaponry-thingy.lua
@@ -1,6 +1,3 @@
--- settings
-local aimPart = "head" -- [HRP, HEAD] (can be cAsE sENsItIvE)
-local fireCount = 7 -- multiplies bullets
 -- services
 local guiService = game:GetService("GuiService")
 local players = game:GetService("Players")
@@ -21,14 +18,15 @@ local function getClientFramework()
 		end
 	end
 end
-local function checkPlr(plr)
-	return plr == player and (plr.TeamColor == player.TeamColor) and not (plr.Character and plr.Character:FindFirstChild("HumanoidRootPart") and not plr.Character:FindFirstChildWhichIsA("ForceField"))
+local function checkPlr(plrArg)
+	local plrHrp, plrHumanoid = plrArg.Character:FindFirstChild("HumanoidRootPart"), plrArg.Character:FindFirstChildWhichIsA("Humanoid")
+	return plrArg ~= player and (plrArg.Team ~= player.Team) and (plrArg.Character and plrHrp and (plrHumanoid and plrHumanoid.Health ~= 0) and not plrArg.Character:FindFirstChildWhichIsA("ForceField"))
 end
 local function getNearestPlrByCursor()
 	local nearPlrs = table.create(0)
 	for _, plr in ipairs(players:GetPlayers()) do
 		local p_char = plr.Character
-		if not checkPlr(plr) and p_char then
+		if checkPlr(plr) and p_char then
 			local p_dPart = p_char:FindFirstChild("HumanoidRootPart")
 			local posVec3, _unused = camera:WorldToScreenPoint(p_dPart.Position)
 			local mouseVec2, posVec2 = Vector2.new(mouse.X, mouse.Y), Vector2.new(posVec3.X, posVec3.Y)
@@ -46,30 +44,133 @@ local function getNearestPlrByCursor()
 end
 -- variables
 local clientFwUpvals = getsenv(getClientFramework()).CheckIsToolValid
+local ui_library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
+-- config
+local config = {
+	["AlwaysAuto"] = false,
+	["InfAmmo"] = false,
+	["NoRecoil"] = false,
+	["NoSpread"] = false,
+	["MultipleBullets"] = {
+		["Enabled"] = false,
+		["AmmoCount"] = 1
+	},
+	["SilentAim"] = {
+		["Enabled"] = false,
+		["AimPart"] = "head"
+	},
+}
+-- ui init
+local mainUI = ui_library:CreateWindow({
+	["Name"] = "Weaponry Fucker",
+	["DefaultTheme"] = [[{"__Designer.Background.UseBackgroundImage":true,"__Designer.Colors.unhoveredOptionBottom":"232323","__Designer.Colors.otherElementText":"817F81","__Designer.Colors.elementText":"939193","__Designer.Colors.hoveredOptionBottom":"2D2D2D","__Designer.Colors.hoveredOptionTop":"414141","__Designer.Colors.section":"B0AFB0","__Designer.Colors.bottomGradient":"1D1D1D","__Designer.Background.ImageTransparency":100,"__Designer.Colors.innerBorder":"493F49","__Designer.Colors.outerBorder":"0F0F0F","__Designer.Colors.sectionBackground":"232222","__Designer.Settings.ShowHideKey":"Enum.KeyCode.RightShift","__Designer.Colors.unhoveredOptionTop":"323232","__Designer.Colors.selectedOption":"373737","__Designer.Colors.background":"282828","__Designer.Background.ImageAssetID":"rbxassetid://4427304036","__Designer.Files.WorkspaceFile":"Weaponry Fucker","__Designer.Colors.unselectedOption":"282828","__Designer.Colors.main":"rainbow","__Designer.Colors.elementBorder":"141414","__Designer.Background.ImageColor":"FFFFFF","__Designer.Colors.topGradient":"232323"}]],
+	["Themeable"] = {
+		["Credit"] = true,
+		["Info"] = "Script made by jLn0n#1464"
+	}
+})
+
+local mainTab = mainUI:CreateTab({
+	["Name"] = "Main Tab"
+})
+
+local weaponMods = mainTab:CreateSection({
+	["Name"] = "Weapon Mods"
+})
+weaponMods:AddToggle({
+	["Name"] = "Always Auto",
+	["Flag"] = "wMods_alwaysAuto",
+	["Callback"] = function(boolToggle)
+		config.AlwaysAuto = boolToggle
+	end
+})
+weaponMods:AddToggle({
+	["Name"] = "Infinite Ammo",
+	["Flag"] = "wMods_infAmmo",
+	["Callback"] = function(boolToggle)
+		config.InfAmmo = boolToggle
+	end
+})
+weaponMods:AddToggle({
+	["Name"] = "No Recoil",
+	["Flag"] = "wMods_noRecoil",
+	["Callback"] = function(boolToggle)
+		config.NoRecoil = boolToggle
+	end
+})
+weaponMods:AddToggle({
+	["Name"] = "No Spread",
+	["Flag"] = "wMods_noSpread",
+	["Callback"] = function(boolToggle)
+		config.NoSpread = boolToggle
+	end
+})
+weaponMods:AddToggle({
+	["Name"] = "Multiple Bullets",
+	["Flag"] = "wMods_mulBulletsToggle",
+	["Callback"] = function(boolToggle)
+		config.MultipleBullets.Enabled = boolToggle
+	end
+})
+weaponMods:AddSlider({
+	["Name"] = "Bullets Count",
+	["Flag"] = "wMods_mulBulletsSlider",
+	["Percision"] = 1,
+	["Min"] = 1,
+	["Max"] = 25,
+	["Callback"] = function(value)
+		config.MultipleBullets.AmmoCount = value
+	end
+})
+
+local silentAim = mainTab:CreateSection({
+	["Name"] = "Aim Settings"
+})
+silentAim:AddToggle({
+	["Name"] = "Silent Aim",
+	["Flag"] = "silentAimToggle",
+	["Callback"] = function(boolToggle)
+		config.SilentAim.Enabled = boolToggle
+	end
+})
+silentAim:AddDropdown({
+	["Name"] = "Aim Part",
+	["Flag"] = "silentAimDropdown",
+	["List"] = {
+		"Head",
+		"HumanoidRootPart"
+	},
+	["Filter"] = {[0] = true},
+	["Value"] = "Head",
+	["Callback"] = function(value)
+		config.SilentAim.AimPart = value
+	end
+})
 -- main
 runService.RenderStepped:Connect(function()
 	local weaponsData = debug.getupvalue(clientFwUpvals, 1)
 	for _, weaponData in pairs(weaponsData) do
 		if weaponData.FriendlyName ~= "Knife" then
-			weaponData.CurrentAmmo = weaponData.WeaponStats.MaxAmmo -- infinite ammo
-			weaponData.CurrentAccuracy = 0 -- no spread
-			weaponData.WeaponStats.FireMode = {
-				["Name"] = "Auto", -- always auto
-				["Round"] = fireCount or 1 -- modifiable bullets
-			}
+			if config.InfAmmo then weaponData.CurrentAmmo = weaponData.WeaponStats.MaxAmmo end -- infinite ammo
+			if config.NoSpread then weaponData.CurrentAccuracy = 0 end -- no spread
+			if config.AlwaysAuto then weaponData.WeaponStats.FireMode.Name = "Auto" end -- always auto
+			if config.MultipleBullets.Enabled then weaponData.WeaponStats.FireMode.Round = config.MultipleBullets.AmmoCount end -- multiple bullets
 		end
 	end
 end)
-local oldCastMouse = rayCastClient.CastRayMouse
-rayCastClient.CastRayMouse = function(_camera, x, y) -- silent aim
-	local nearestPlr = getNearestPlrByCursor()
-	if nearestPlr then
-		aimPart = string.lower(aimPart)
-		local p_char = nearestPlr.Character
-		local p_targetPart = p_char:FindFirstChild(aimPart == "hrp" and "HumanoidRootPart" or aimPart == "head" and "Head" or nil)
-		local newVec2 = camera:WorldToScreenPoint(p_targetPart.Position)
-		x, y = newVec2.X, newVec2.Y + guiService:GetGuiInset().Y
+local oldCastMouse, oldRecoilFunc = rayCastClient.CastRayMouse, recoilHandler.accelerate
+rayCastClient.CastRayMouse = function(_camera, x, y)
+	if config.SilentAim.Enabled then -- silent aim
+		local nearestPlr = getNearestPlrByCursor()
+		if nearestPlr and checkPlr(nearestPlr) then
+			local p_char = nearestPlr.Character
+			local p_targetPart = p_char:FindFirstChild(config.SilentAim.AimPart)
+			local newVec2 = camera:WorldToScreenPoint(p_targetPart.Position)
+			x, y = newVec2.X, newVec2.Y + guiService:GetGuiInset().Y
+		end
 	end
 	return oldCastMouse(_camera, x, y)
 end
-recoilHandler.accelerate = function()end -- no recoil
+recoilHandler.accelerate = function(...)
+	return not config.NoRecoil and oldRecoilFunc(...) or nil -- no recoil
+end
