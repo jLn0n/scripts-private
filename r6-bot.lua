79a5facd7727fbdde21acc9ed2fd1000fd2b92dc
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-12-07 12:51:12 +0800

    HOW THE FUCK AM I GONNA DO THE CLAIMING PART

--- r6-bot.lua
+++ r6-bot.lua
@@ -6,22 +6,22 @@ local starterGui = game:GetService("StarterGui")
 local player = players.LocalPlayer
 local character = player.Character
 local humanoid = character.Humanoid
-local hRootPart = character.HumanoidRootPart
+local rootPart = character.HumanoidRootPart
 -- init
 assert(not character.Parent:FindFirstChild(string.format("%s-reanimation", player.UserId)), string.format([[\n["R6-BOT.LUA"]: Please reset to be able to run the script again]]))
 assert(humanoid.RigType == Enum.HumanoidRigType.R6, string.format([[\n["R6-BOT.LUA"]: Sorry, This script will only work on R6 character rig]]))
 do -- config initialization
 	_G.Connections, _G.Settings = _G.Connections or table.create(0), _G.Settings or table.create(0)
-	_G.Settings.HeadName = _G.Settings.HeadName or "NinjaMaskOfShadows"
+	_G.Settings.HeadName = _G.Settings.HeadName or "MediHood"
 	_G.Settings.Velocity = _G.Settings.Velocity or Vector3.new(-35, -25.05, 0)
 	_G.Settings.RemoveHeadMesh = _G.Settings.RemoveHeadMesh == nil and false or _G.Settings.RemoveHeadMesh
 	_G.Settings.UseBuiltinNetless = _G.Settings.UseBuiltinNetless == nil or true or _G.Settings.UseBuiltinNetless
 end
 for _, connection in ipairs(_G.Connections) do connection:Disconnect() end table.clear(_G.Connections)
 -- variables
-local charOldPos = hRootPart.CFrame
+local charOldPos = rootPart.CFrame
 local accessories, bodyParts = table.create(0), {
-	["Head"] = (character:FindFirstChild(_G.Settings.HeadName) or character:FindFirstChild("MediHood")),
+	["Head"] = character:FindFirstChild(_G.Settings.HeadName),
 	["Torso"] = character:FindFirstChild("SeeMonkey"),
 	["Torso1"] = character:FindFirstChild("Robloxclassicred"),
 	["Torso2"] = character:FindFirstChild("LavanderHair"),
@@ -44,16 +44,14 @@ local function onCharRemoved()
 end
 
 task.defer(function() -- initializing reanimation after the code below ran
-	character:SetPrimaryPartCFrame(CFrame.new(Vector3.new(1, 1, 1) * 10e10))
-	task.wait(.175)
-	hRootPart.Anchored = true
+	rootPart.Anchored = true
 	local animScript, plrFace = character.Animate, character.Head.face:Clone()
 	humanoid.Animator:Clone().Parent = botChar.Humanoid
 	animScript.Disabled = true
 	animScript.Parent = botChar
 	animScript.Disabled = false
-	plrFace.Parent, plrFace.Transparency = botChar.Head, 1
 	botChar.HumanoidRootPart.CFrame = charOldPos
+	plrFace.Parent, plrFace.Transparency = botChar.Head, 1
 	for PartName, object in pairs(bodyParts) do
 		if object and object:FindFirstChild("Handle") then
 			object.Name = (string.match(PartName, "Torso") and "Torso" or PartName)
@@ -87,39 +85,39 @@ task.defer(function() -- initializing reanimation after the code below ran
 	})
 end)
 
-if _G.Settings.UseBuiltinNetless then player:GetPropertyChangedSignal("Character"):Wait()
+if _G.Settings.UseBuiltinNetless then
 	settings().Physics.AllowSleep = false
 	settings().Physics.ThrottleAdjustTime = 0 / 0
-	settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.DefaultAuto
-	player.ReplicationFocus = workspace
+	settings().Physics.PhysicsEnvironmentalThrottle = Enum.EnviromentalPhysicsThrottle.Skip8
 
+	_G.Connections[#_G.Connections + 1] = runService.Heartbeat:Connect(function()
+		for _, object in ipairs(character:GetChildren()) do
+			object = (object:IsA("Accessory") and object:FindFirstChild("Handle") or nil)
+			if object then
+				object.Massless, object.CanCollide = true, false
+				object.Velocity, object.RotVelocity = _G.Settings.Velocity, Vector3.new()
+				sethiddenproperty(object, "NetworkIsSleeping", false)
+			end
+		end
+		setsimulationradius(-1, -1)
+	end)
+
+	player:GetPropertyChangedSignal("Character"):Wait()
 	for _, object in ipairs(character:GetChildren()) do
-		if object:IsA("Accessory") and object:FindFirstChild("Handle") then object = object:FindFirstChild("Handle")
-			local BodyVel, BodyAngVel = Instance.new("BodyForce"), Instance.new("BodyAngularVelocity")
-			BodyVel.Force = _G.Settings.Velocity
+		object = (object:IsA("Accessory") and object:FindFirstChild("Handle") or nil)
+		if object then
+			local BodyVel, BodyAngVel = Instance.new("BodyVelocity"), Instance.new("BodyAngularVelocity")
+			BodyVel.MaxForce, BodyVel.Velocity = _G.Settings.Velocity, _G.Settings.Velocity
 			BodyAngVel.MaxTorque, BodyAngVel.AngularVelocity = Vector3.new(), Vector3.new()
 			BodyVel.Parent, BodyAngVel.Parent = object, object
 		end
 	end
-
-	-- TODO: disconnect the coroutine when its no longer used
-	coroutine.resume(coroutine.create(function()
-		while true do
-			for _, object in ipairs(character:GetChildren()) do
-				if object:IsA("Accessory") and object:FindFirstChild("Handle") then object = object:FindFirstChild("Handle")
-					object.Massless, object.CanCollide = true, false
-					object.Velocity, object.RotVelocity = _G.Settings.Velocity, Vector3.new()
-					sethiddenproperty(object, "NetworkIsSleeping", false)
-				end
-			end
-			task.wait()
-		end
-	end))
 end
 
 _G.Connections[#_G.Connections + 1] = runService.Heartbeat:Connect(function()
 	for PartName, object in pairs(bodyParts) do
-		if object:FindFirstChild("Handle") then object = object:FindFirstChild("Handle")
+		object = (object:IsA("Accessory") and object:FindFirstChild("Handle") or nil)
+		if object then
 			object.LocalTransparencyModifier = botChar.Head.LocalTransparencyModifier
 			if PartName == "Head" then
 				object.CFrame = botChar.Head.CFrame
@@ -133,7 +131,8 @@ _G.Connections[#_G.Connections + 1] = runService.Heartbeat:Connect(function()
 		end
 	end
 	for _, object in pairs(accessories) do
-		if object:FindFirstChild("Handle") then object = object:FindFirstChild("Handle")
+		object = object:FindFirstChild("Handle")
+		if object then
 			object.LocalTransparencyModifier = botChar.Head.LocalTransparencyModifier
 			object.CFrame = botChar:FindFirstChild(object.Parent.Name).Handle.CFrame
 		end
