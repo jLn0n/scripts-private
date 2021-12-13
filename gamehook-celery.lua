a3c967b5d23f1f8a98f9396eda9e44826d91617e
jLn0n <45232009+jLn0n@users.noreply.github.com>
2021-12-13 16:28:53 +0800

    args and bullshit fixes

--- gamehook-celery.lua
+++ gamehook-celery.lua
@@ -1,34 +1,32 @@
-local insertService = game:GetService("InsertService");
-
-local gameMt = getrawmetatable(game);
-local gameRealIndex = gameMt.__index;
-local gameRealNameCall = gameMt.__namecall;
-
-setreadonly(gameMt, false);
-
+-- services
+local insertService = game:GetService("InsertService")
+-- variables
+local gameMt = getrawmetatable(game)
+local oldIndex, oldNamecall = gameMt.__index, gameMt.__namecall
+-- main
+setreadonly(gameMt, false)
 gameMt.__index = function(...)
-	local args = {...}
-	local nameIndex = args[2]
-	if nameIndex == "HttpGet" then
-		local url, isAsync = args[3], args[4]
-		return httpget(url, isAsync)
-	elseif nameIndex == "GetObjects" then
-		return { insertService:LoadLocalAsset(args[3]) }
+	if checkcaller() then
+		local args = {...}
+		local nameIndex = args[2]
+		if nameIndex == "HttpGet" then
+			return httpget(args[3], args[4])
+		elseif nameIndex == "GetObjects" then
+			return {insertService:LoadLocalAsset(args[3])}
+		end
 	end
-	return gameRealIndex(...);
+	return oldIndex(...)
 end
-
 gameMt.__namecall = function(...)
 	if checkcaller() then
 		local args = {...}
-		local namecallMethod = getnamecallmethod();
+		local namecallMethod = getnamecallmethod()
 		if namecallMethod == "HttpGet" then
-			return httpget(args[1], args[2])
+			return httpget(args[2], args[3])
 		elseif namecallMethod == "GetObjects" then
-			return {insertService:LoadLocalAsset(args[1])}
+			return {insertService:LoadLocalAsset(args[2])}
 		end
 	end
-	return gameRealNameCall(...);
+	return oldNamecall(...)
 end
-
-setreadonly(gameMt, true);
+setreadonly(gameMt, true)
