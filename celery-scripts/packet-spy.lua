62196066e6ad476d3b0c548c034db0f02937c71a
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-02-03 16:04:12 +0800

    removed useless packet thingy idk what

--- celery-scripts/packet-spy.lua
+++ celery-scripts/packet-spy.lua
@@ -1,63 +1,60 @@
 -- variables
-local packetIdList = { -- gathered from https://github.com/Arisstath/roblox-dissector/blob/master/peer/RakNetLayer.go#L74
-	0x81, -- "ID_SET_GLOBALS"
-	0x82, -- "ID_TEACH_DESCRIPTOR_DICTIONARIES"
-	0x83, -- "ID_DATA"
-	0x84, -- "ID_MARKER"
-	0x85, -- "ID_PHYSICS"
-	0x86, -- "ID_TOUCHES"
-	0x87, -- "ID_CHAT_ALL"
-	0x88, -- "ID_CHAT_TEAM"
-	0x89, -- "ID_REPORT_ABUSE"
-	0x8A, -- "ID_SUBMIT_TICKET"
-	0x8B, -- "ID_CHAT_GAME"
-	0x8C, -- "ID_CHAT_PLAYER"
-	0x8D, -- "ID_CLUSTER"
-	0x8E, -- "ID_PROTOCOL_MISMATCH"
-	0x8F, -- "ID_PREFERRED_SPAWN_NAME"
-	0x90, -- "ID_PROTOCOL_SYNC"
-	0x91, -- "ID_SCHEMA_SYNC"
-	0x92, -- "ID_PLACEID_VERIFICATION"
-	0x93, -- "ID_DICTIONARY_FORMAT"
-	0x94, -- "ID_HASH_MISMATCH"
-	0x95, -- "ID_SECURITYKEY_MISMATCH"
-	0x96, -- "ID_REQUEST_STATS"
-	0x97, -- "ID_NEW_SCHEMA"
+local packetIdList = {
+	["1B"] = "ID_PHYSICS",
+	["83"] = "ID_DATA",
 }
-local whitelistedPackets = {
+local packetsToFind = {
 	0x83,
-	0x85
+	0x03,
+	0x01
 }
-local strResult = ""
+local strResult, packetSpyEnabled = "", false
 -- functions
-local function compilePacketPayload(packetPayload)
+local function compilePacketData(packetData)
 	local result = "\"%s\""
-	for index, packet in ipairs(packetPayload) do
-		result = string.format(result, string.format("%02X", packet) .. (index == #packetPayload and "" or " %s"))
+	for index, packet in ipairs(packetData) do
+		result = string.format(result, string.format("%02X", packet) .. (index == #packetData and "" or " %s"))
 	end
 	return result
 end
 local function createPacketOutput(packetData)
 	local result = ""
 	-- compiled_packet
-	result = result .. string.format("compiled_packet: %s\n", compilePacketPayload(packetData))
+	result ..= string.format("compiled_packet: %s\n", compilePacketData(packetData))
 	-- packet_data
-	result = result .. "packet_data:\n"
-	for index, value in ipairs(packetData) do
-		result = result .. string.format("  %s: %s\n", index, string.format("0x%02X (%s)", value, value) .. (table.find(packetIdList, value) and " - packet_id" or ""))
+	result ..= "packet_data:\n"
+	for index, packet in ipairs(packetData) do
+		local packetHex = string.format("%02X", packet)
+		result ..= string.format("  %s: %s\n", index, string.format("0x%s (%s)", packetHex, packet) .. (packetIdList[packetHex] and string.format(" - packet-id: %s", packetIdList[packetHex]) or ""))
 	end
 	-- seperator
-	result = result .. string.rep("----", 40) .. "\n"
+	result ..= string.rep("----", 40) .. "\n"
 	return result
 end
--- main
-rnet.Capture:Connect(function(packet)
-	if table.find(whitelistedPackets, packet.id) then
-		strResult = strResult .. createPacketOutput(packet.data)
+local function packetsFound(packetData)
+	local packetIndex, foundPackets = 0, 0
+	for _, packet in ipairs(packetData) do
+		packetIndex += 1
+		if packetsToFind[packetIndex] == packet then
+			foundPackets += 1
+		end
 	end
-end)
-game.Close:Connect(function()
-	writefile("packet-output.txt", strResult)
-	task.wait(2.5)
-end)
+	return foundPackets == #packetsToFind
+end
+-- main
 print("packet-spy.lua running!")
+getgenv()._G.packetSpyToggle = function()
+	packetSpyEnabled = not packetSpyEnabled
+	print("packet spy is", (packetSpyEnabled and "enabled" or "disabled"))
+	if not packetSpyEnabled then
+		writefile("packet-output.txt", strResult)
+	end
+end
+while true do task.wait()
+	if packetSpyEnabled then
+		local packetData = rnet.getpacket()
+		if packetsFound(packetData) then
+			strResult ..= createPacketOutput(packetData)
+		end
+	end
+end
