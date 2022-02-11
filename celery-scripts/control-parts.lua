c7e327494c20a5461c302e085f705f18973d848a
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-02-11 20:45:26 +0800

    thingy changes

--- celery-scripts/control-parts.lua
+++ celery-scripts/control-parts.lua
@@ -4,7 +4,11 @@ local inputService = game:GetService("UserInputService")
 -- objects
 local player = players.LocalPlayer
 local mouse = player:GetMouse()
-local partThingy, controlledPart, controlledPartPos
+local partHolder = Instance.new("Part")
+local partThingy, controlledPart, currentPickedPos
+local cPartBodyPos, cPartBodyGyro
+-- variables
+local nonceCFrame = CFrame.new()
 -- function
 local function initFloaties(object)
 	if not object then return end
@@ -12,52 +16,50 @@ local function initFloaties(object)
 		if not (_object:IsA("LuaSourceContainer") or string.find(_object.ClassName, "Body")) then continue end
 		_object:Destroy()
 	end
-	local bodyPos = Instance.new("BodyPosition")
-	bodyPos.D = 150000
-	bodyPos.MaxForce = Vector3.one * 4e6
-	bodyPos.P = 1e6
-	bodyPos.Parent = object
+	local bodyPos, bodyGyro = Instance.new("BodyPosition"), Instance.new("BodyGyro")
+	bodyPos.D, bodyGyro.D = 50, 300
+	bodyPos.MaxForce, bodyGyro.MaxTorque = Vector3.one * math.huge, Vector3.one * math.huge
+	bodyPos.P, bodyGyro.P = 5000, 5000
+	bodyPos.Parent, bodyGyro.Parent = object, object
+	return bodyPos, bodyGyro
 end
 -- main
+partHolder.Anchored, partHolder.Transparency, partHolder.Size, partHolder.Parent = true, .5, Vector3.new(2.5, .05, 2.5), workspace
 inputService.InputBegan:Connect(function(input)
 	if not inputService:GetFocusedTextBox() then
 		if input.UserInputType == Enum.UserInputType.Keyboard then
 			if input.KeyCode == Enum.KeyCode.KeypadZero then
-				controlledPart = nil
+				controlledPart, cPartBodyPos, cPartBodyGyro = nil, nil, nil
 			elseif input.KeyCode == Enum.KeyCode.KeypadOne then
-				for _, object in ipairs(player.Character:GetChildren()) do
-					if object:IsA("Tool") then
-						object.Parent = workspace
-					end
-				end
+				player.Character:MoveTo(controlledPart.Position)
 			end
 		elseif input.UserInputType == Enum.UserInputType.MouseButton1 then
 			if inputService:IsKeyDown(Enum.KeyCode.X) then
 				local targetPart = mouse.Target
 				if partThingy and targetPart and not targetPart.Anchored and not targetPart:FindFirstChildWhichIsA("Weld", true) then
-					controlledPart, controlledPartPos = targetPart, partThingy.Position
-					targetPart.CanCollide = false
+					cPartBodyPos, cPartBodyGyro = initFloaties(targetPart)
+					controlledPart, currentPickedPos = targetPart, partThingy.Position
 					for _ = 1, 5 do
 						rnet.sendposition(targetPart.Position)
-						targetPart.Position = controlledPartPos
+						targetPart.Position = currentPickedPos
 						rnet.sendposition(partThingy.Position)
 						task.wait()
 					end
-					initFloaties(targetPart)
 					player.Character:MoveTo(partThingy.Position)
-					targetPart.CanCollide = true
 				end
 			elseif inputService:IsKeyDown(Enum.KeyCode.Z) and controlledPart then
-				controlledPartPos = mouse.Hit.Position
+				currentPickedPos = mouse.Hit.Position
 			end
 		end
 	end
 end)
 while true do task.wait()
 	partThingy = player.Character and player.Character:FindFirstChild("HumanoidRootPart") or nil
-	if controlledPart and controlledPart:IsDescendantOf(workspace) then
-		controlledPart:FindFirstChildWhichIsA("BodyPosition").Position = controlledPartPos
+	if controlledPart and controlledPart:IsDescendantOf(workspace) and (cPartBodyPos and cPartBodyGyro) then
+		cPartBodyPos.Position, cPartBodyGyro.CFrame = currentPickedPos, nonceCFrame
+		partHolder.Position, partHolder.Size = controlledPart.Position + (Vector3.yAxis * ((controlledPart.Size.Y + partHolder.Size.Y) / 2)), Vector3.new(controlledPart.Size.X, .05, controlledPart.Size.Z)
 		controlledPart.Velocity, controlledPart.RotVelocity = (Vector3.yAxis * 25.05), Vector3.zero
-		controlledPart.Velocity, controlledPart.RotVelocity = -(Vector3.yAxis * 20.05), Vector3.zero
+		controlledPart.AssemblyAngularVelocity, controlledPart.AssemblyLinearVelocity = Vector3.zero, Vector3.zero
+		controlledPart.CustomPhysicalProperties = PhysicalProperties.new(0, 0, 0, 0, 0)
 	end
 end
