31838045ce7e9488b9bc3af5a650e21a9e87006b
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-03-15 21:32:51 +0800

    dayum boi it works now
    * added junk code generation
    * some shitty improvements

--- misc/simbuscator.lua
+++ misc/simbuscator.lua
@@ -3,19 +3,23 @@
 	just a simple obfuscator without any protections in lua
 --]]
 -- variablesc
-local hexBytes = table.create(0)
 local obfTemplates = {
 	["varsList"] = [==[local {fenv}=loadstring("{fenv_return}")();local {string_gsub}={fenv}("{gsub_str}");local {string_char}={fenv}("{char_str}");local {tostring}={fenv}("{tostr_str}");local {tonumber}={fenv}("{tonum_str}");local {loadstring}={fenv}("{loadstr_str}");local {math_min}={fenv}("{mmin_str}");]==],
 	["minifiedHexToStr"] = [==[local function {hextostr_func}({hextostr_func_arg1}) return {string_gsub}({hextostr_func_arg1}, "{enc_str1}", function({func_inner1_arg1}) return {string_char}(({tonumber}({func_inner1_arg1}, {enc_number1}) or 0) / {dec_offsetint}) end) end;]==],
 	["loadstringProxy"] = [==[local function {loadstr_proxy_func}({loadstr_proxy_func_arg1}) return {loadstring}({string_gsub}({hextostr_func}({loadstr_proxy_func_arg1}), "|", ""), "{source_name}")() end;]==],
 	["loadstringScript"] = [==[{loadstr_proxy_func}("{source}");]==],
 	junkCodes = {
-		[==[local function {var_name}() return ({integer} == {integer}) end;]==],
+		[==[local function {var_name}() return ({integer} == {integer_enc1}) end;]==],
 		[==[local function {var_name}({var_name_arg1}, {var_name_arg2}) return ({integer} * ({var_name_arg1} / {var_name_arg2})) end;]==],
 		[==[local {var_name}={var_result};]==],
 	}
 }
-local strFenvGet = [==[return function(strArg)local seps,result=string.split(strArg, "."),getfenv() for index = 1, #seps do result = result[seps[index]] end return result end]==]
+local constantTypeChoices = {
+	"number",
+	"bool",
+	"string",
+}
+local strFenvGet = [==[return function(a)local b,c=string.split(a, "."),getfenv()for d = 1, #b do c = c[b[d]] end return c end]==]
 local stringList = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
 -- functions
 local function generateRandString(lenght)
@@ -34,23 +38,36 @@ local function stringToHex(stringArg, seperator, offset)
 	end)
 end
 local function scrambleNumber(number)
-	return string.format("{math_min}(((%s / 2) * 8 / 5) * (%s / 2), 0x%02X)", number, number, number)
+	return string.format("{math_min}(((%s / 2) * (8 / 5)) * (%s * (69 / 34.5)), 0x%02X)", number, number, number)
 end
-local function formatTemplate(templateName, outputs)
-	if (not templateName or not obfTemplates[templateName]) or not outputs then return end
-	local stringResult = obfTemplates[templateName]
-	for name, value in pairs(outputs) do
-		stringResult = string.gsub(stringResult, string.format("{%s}?", name), value)
+local function formatString(templateStr, options, func)
+	local stringResult = templateStr
+	for _name in pairs(options) do
+		stringResult = string.gsub(stringResult, string.format("{%s?}", _name), function(name)
+			name = string.gsub(name, "{", "")
+			name = string.gsub(name, "}", "")
+			return (typeof(func) == "function" and func(name) or options[name])
+		end)
 	end
 	return stringResult
 end
-local function generateJunkCode() -- TODO: generate junkcodes according to the plan
-	local stringResult = obfTemplates.junkCodes[math.random(1, #obfTemplates.junkCodes)]
-	string.gsub(stringResult, "{%s}?", function(value)
-		if string.find(value, "integer") then
-			return scrambleNumber(math.random(1, 50))
-		end
-	end)
+local function generateRandomConstant()
+	local stringResult = ""
+	local pickedConstantType = constantTypeChoices[math.random(1, #constantTypeChoices)]
+	if pickedConstantType == "string" then
+		stringResult ..= "\"" .. stringToHex(generateRandString(math.random(1, 128))) .. "\""
+	elseif pickedConstantType == "bool" then
+		stringResult ..= (math.random(1, 8) <= 4 and "true" or "false")
+	elseif pickedConstantType == "number" then
+		local randPickedInt = math.random(1, 4)
+		local randInt = math.random(1, 256)
+		stringResult ..= (
+			if randPickedInt == 1 then randInt
+			elseif randPickedInt == 2 then "0x" .. stringToHex(randInt, "")
+			elseif randPickedInt == 3 then scrambleNumber(randInt)
+			else "\"" .. stringToHex(randInt) .. "\""
+		)
+	end
 	return stringResult
 end
 local function mergeDictionary(table1, table2)
@@ -63,12 +80,28 @@ local function mergeDictionary(table1, table2)
 	end
 	return table1
 end
+local function generateJunkCode(addedOptions)
+	local stringResult = obfTemplates.junkCodes[math.random(1, #obfTemplates.junkCodes)]
+	local generatedOptions = mergeDictionary(addedOptions, {
+		var_name = generateRandString(12),
+		var_result = "",
+		var_name_arg1 = generateRandString(8),
+		var_name_arg2 = generateRandString(8),
+		integer = math.random(1, 64),
+		integer_enc1 = scrambleNumber(math.random(1, 64)),
+	})
+	stringResult = formatString(stringResult, generatedOptions, function(optionName)
+		return (optionName == "var_result" and generateRandomConstant() or nil)
+	end)
+	return stringResult
+end
 local function obfuscateScript(outputArg, sourceName)
+	math.randomseed(os.clock())
 	outputArg = string.gsub(outputArg, "\t", "")
 	local resultData = table.create(0)
 	local obfResult = ""
 	local offsetInt = math.random(8, 32) * 8
-	local varsListOutput, minHexToStrOutput, lstrPrxyOutput, lstrMainOutput = {
+	local generatedOptions = {
 		string_char = generateRandString(12),
 		string_gsub = generateRandString(12),
 		fenv = generateRandString(12),
@@ -83,29 +116,27 @@ local function obfuscateScript(outputArg, sourceName)
 		loadstr_str = stringToHex("loadstring"),
 		tonum_str = stringToHex("tonumber"),
 		tostr_str = stringToHex("tostring"),
-	}, {
 		hextostr_func = generateRandString(12),
 		hextostr_func_arg1 = generateRandString(8),
 		func_inner1_arg1 = generateRandString(8),
 		enc_str1 = stringToHex("[%x]+"),
 		enc_number1 = scrambleNumber(16),
-		dec_offsetint = tostring(offsetInt)
-	}, {
+		dec_offsetint = tostring(offsetInt),
 		loadstr_proxy_func = generateRandString(12),
 		loadstr_proxy_func_arg1 = generateRandString(8),
-		source_name = stringToHex(sourceName)
-	}, {
-		source = stringToHex(outputArg, "|", offsetInt)
+		source_name = stringToHex(sourceName),
+		source = stringToHex(outputArg, "|", offsetInt),
 	}
-	for _ = 1, math.random(4, 16) do
-		table.insert(resultData, math.random(1, 3), generateJunkCode())
+	for _ = 1, (math.random(1, 8) * 4) do
+		local tablePos = math.random(1, 4) + 1
+		table.insert(resultData, tablePos, generateJunkCode(generatedOptions))
 	end
-	table.insert(resultData, formatTemplate("varsList", varsListOutput))
-	table.insert(resultData, formatTemplate("minifiedHexToStr", mergeDictionary(varsListOutput, minHexToStrOutput)))
-	table.insert(resultData, formatTemplate("loadstringProxy", mergeDictionary(varsListOutput, mergeDictionary(minHexToStrOutput, lstrPrxyOutput))))
-	table.insert(resultData, #resultData + 1, formatTemplate("loadstringScript", mergeDictionary(lstrPrxyOutput, lstrMainOutput)))
+	table.insert(resultData, 1, formatString(obfTemplates.varsList, generatedOptions))
+	table.insert(resultData, 2, formatString(obfTemplates.minifiedHexToStr, generatedOptions))
+	table.insert(resultData, 3, formatString(obfTemplates.loadstringProxy, generatedOptions))
+	table.insert(resultData, (math.random(1, #resultData) + 3), formatString(obfTemplates.loadstringScript, generatedOptions))
 	for index = 1, #resultData do
-		obfResult ..= resultData[index]
+		obfResult ..= resultData[index] or ""
 	end
 	return obfResult
 end
