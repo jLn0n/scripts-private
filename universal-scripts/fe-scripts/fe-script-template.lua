872a13fed5f1f59b6abd43bb3f2070ae3aec2813
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-03-22 21:19:29 +0800

    Create fe-script-template.lua

---
+++ universal-scripts/fe-scripts/fe-script-template.lua
@@ -0,0 +1,48 @@
+-- services
+local inputService = game:GetService("UserInputService")
+local players = game:GetService("Players")
+local runService = game:GetService("RunService")
+-- objects
+local player = players.LocalPlayer
+local character = player.Character
+local rootPart, torso = character:FindFirstChild("HumanoidRootPart"), character:FindFirstChild("Torso")
+-- variables
+local states = {
+	["animPlaying"] = "idle",
+	["isAttacking"] = false,
+	["mode"] = 1,
+}
+local motorsList
+-- functions
+local function createMotorObj(motorObj)
+	return {
+		["Object"] = motorObj,
+		["CFrame"] = motorObj.Transform,
+		["Cache"] = motorObj.Transform
+	}
+end
+local function changeMotorCFrame(motorName, cframe)
+	if not (motorName and motorsList[motorName]) or cframe then return end
+	motorsList[motorName].Object = motorsList.Cache * cframe
+end
+-- main
+motorsList = {
+	["Neck"] = createMotorObj(torso:FindFirstChild("Neck")),
+	["LShoulder"] = createMotorObj(torso:FindFirstChild("Left Shoulder")),
+	["RShoulder"] = createMotorObj(torso:FindFirstChild("Right Shoulder")),
+	["LHip"] = createMotorObj(torso:FindFirstChild("Left Hip")),
+	["RHip"] = createMotorObj(torso:FindFirstChild("Right Hip")),
+	["RootJoint"] = createMotorObj(rootPart:FindFirstChild("RootJoint")),
+}
+
+_G.Connections[#_G.Connections + 1] = inputService.InputBegan:Connect(function(input, gameProcessed)
+	if input.UserInputType == Enum.UserInputType.Keyboard and (not inputService:GetFocusedTextBox() and gameProcessed) then
+
+	end
+end)
+
+_G.Connections[#_G.Connections + 1] = runService.Heartbeat:Connect(function()
+	for _, motorObj in pairs(motorsList) do
+		motorObj.Object.Transform = motorObj.Object.Transform:Lerp(motorObj.CFrame, .25)
+	end
+end)
