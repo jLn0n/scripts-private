98cabc8fc048c108674594e928d7af28114befa1
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-03-25 00:25:19 +0800

    this doesn't work for now

--- celery-scripts/control-parts.lua
+++ celery-scripts/control-parts.lua
@@ -5,62 +5,64 @@ local inputService = game:GetService("UserInputService")
 local player = players.LocalPlayer
 local mouse = player:GetMouse()
 local partHolder = Instance.new("Part")
-local partThingy, controlledPart, currentPickedPos
-local cPartBodyPos, cPartBodyGyro
+local rootPart, controlledPart, setControlledPart, partPos
 -- function
-local function initFloaties(object)
-	if not object then return end
-	for _, _object in ipairs(object:GetChildren()) do
+local function setControlling(partObj)
+	setControlledPart = partObj or rootPart
+	rnet.setphysicsrootpart(setControlledPart)
+end
+local function setPartPosition(partObj, posVec3)
+	if not partObj then return end
+	if setControlledPart ~= partObj then
+		setControlling(partObj)
+	end
+	local cframeThingy = CFrame.new(posVec3 or Vector3.zero)
+	rnet.sendphysics(cframeThingy)
+	--partObj.CFrame = cframeThingy
+end
+local function disableUselessThingys(partObj)
+	if not partObj then return end
+	for _, _object in ipairs(partObj:GetChildren()) do
 		if not (_object:IsA("LuaSourceContainer") or string.find(_object.ClassName, "Body")) then continue end
 		_object:Destroy()
 	end
-	local bodyPos, bodyGyro = Instance.new("BodyPosition"), Instance.new("BodyGyro")
-	bodyPos.D, bodyGyro.D = 25, 300
-	bodyPos.MaxForce, bodyGyro.MaxTorque = Vector3.new(4e5, 4e6, 4e5), Vector3.new(4e5, 4e6, 4e5)
-	bodyPos.P, bodyGyro.P = 1250, 5000
-	bodyPos.Parent, bodyGyro.Parent = object, object
-	return bodyPos, bodyGyro
 end
 -- main
 partHolder.Anchored, partHolder.Transparency, partHolder.Parent = true, .5, workspace
 inputService.InputBegan:Connect(function(input)
 	if not inputService:GetFocusedTextBox() then
 		if input.UserInputType == Enum.UserInputType.Keyboard then
-			if input.KeyCode == Enum.KeyCode.KeypadZero then
-				controlledPart, cPartBodyPos, cPartBodyGyro = nil, nil, nil
-			elseif input.KeyCode == Enum.KeyCode.KeypadOne then
-				player.Character:MoveTo(controlledPart and controlledPart.Position or partThingy.Position)
+			if input.KeyCode == Enum.KeyCode.KeypadOne then
+				controlledPart = nil
+				setControlling(nil)
+			elseif input.KeyCode == Enum.KeyCode.KeypadZero then
+				player.Character:MoveTo(controlledPart and controlledPart.Position or rootPart.Position)
 			end
 		elseif input.UserInputType == Enum.UserInputType.MouseButton1 then
 			if inputService:IsKeyDown(Enum.KeyCode.X) then
 				local targetPart = mouse.Target
-				if partThingy and targetPart and not targetPart.Anchored and #targetPart:GetJoints() == 0 then
-					controlledPart, currentPickedPos = targetPart, partThingy.Position
+				if rootPart and targetPart and not targetPart.Anchored and #targetPart:GetJoints() == 0 then
+					controlledPart, partPos = targetPart, rootPart.Position
+					setControlling(rootPart)
+					disableUselessThingys(controlledPart)
 					for _ = 1, 5 do
 						rnet.sendphysics(CFrame.new(targetPart.Position))
-						targetPart.Position = partThingy.Position
-						rnet.sendphysics(CFrame.new(partThingy.Position))
+						targetPart.Position = rootPart.Position
+						rnet.sendphysics(CFrame.new(rootPart.Position))
 						task.wait()
 					end
-					targetPart.Position = currentPickedPos
-					cPartBodyPos, cPartBodyGyro = initFloaties(targetPart)
-					player.Character:MoveTo(partThingy.Position)
+					setPartPosition(targetPart, partPos)
+					player.Character:MoveTo(rootPart.Position)
 				end
 			elseif inputService:IsKeyDown(Enum.KeyCode.Z) and controlledPart then
-				currentPickedPos = mouse.Hit.Position
-				for _= 1, 5 do task.wait()
-					rnet.sendphysics(CFrame.new(controlledPart.Position))
-				end
+				partPos = mouse.Hit.Position
 			end
 		end
 	end
 end)
 while true do task.wait()
-	partThingy = player.Character and player.Character:FindFirstChild("HumanoidRootPart") or nil
-	if controlledPart and controlledPart:IsDescendantOf(workspace) and (cPartBodyPos and cPartBodyGyro) then
-		cPartBodyPos.Position, cPartBodyGyro.CFrame = currentPickedPos, CFrame.new()
-		partHolder.Position, partHolder.Size = controlledPart.Position - (Vector3.yAxis * ((controlledPart.Size.Y + partHolder.Size.Y) / 2)), Vector3.new(controlledPart.Size.X, .05, controlledPart.Size.Z)
-		controlledPart.Velocity, controlledPart.RotVelocity = (Vector3.yAxis * 25.05), Vector3.zero
-		controlledPart.Velocity, controlledPart.RotVelocity = -(Vector3.yAxis * 20.05), Vector3.zero
+	rootPart = player.Character and player.Character:FindFirstChild("HumanoidRootPart") or nil
+	if controlledPart and controlledPart:IsDescendantOf(workspace) then
+		setPartPosition(controlledPart, partPos)
 	end
 end
