2490b4c527be473f9450969404c8a205f8af5ca5
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-04-18 20:50:27 +0800

    aeaeaeaeaeae

--- universal-scripts/hat-collisions.lua
+++ universal-scripts/hat-collisions.lua
@@ -1,18 +1,19 @@
 -- services
 local players = game:GetService("Players")
+local starterGui = game:GetService("StarterGui")
 -- objects
 local player = players.LocalPlayer
 local character = player.Character
+local resetBindable = Instance.new("BindableEvent")
 -- variables
-local destroyFunc = character.Destroy
+local destroyFunc, resetBindableConnection = character.Destroy, nil
 -- main
 player.Character = nil
 player.Character = character
 task.wait(players.RespawnTime + .05)
 
-task.defer(character.BreakJoints, character)
-task.spawn(destroyFunc, (character:FindFirstChild("Torso") or character:FindFirstChild("UpperTorso")))
-task.spawn(destroyFunc, character.HumanoidRootPart)
+destroyFunc(character:FindFirstChild("Torso") or character:FindFirstChild("UpperTorso"))
+destroyFunc(character.HumanoidRootPart)
 
 for _, object in ipairs(character:GetChildren()) do
 	if object:IsA("Accoutrement") then
@@ -22,3 +23,13 @@ for _, object in ipairs(character:GetChildren()) do
 		destroyFunc(object)
 	end
 end
+
+resetBindableConnection = resetBindable.Event:Connect(function()
+	starterGui:SetCore("ResetButtonCallback", true)
+	resetBindableConnection:Disconnect()
+	local daModel = Instance.new("Model")
+	local _daModelHumanoid = Instance.new("Humanoid")
+	_daModelHumanoid.Parent = daModel
+	player.Character = daModel
+end)
+starterGui:SetCore("ResetButtonCallback", resetBindable)
