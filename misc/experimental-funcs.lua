255c7e70bf06ad6bd0cd3822b494148418d7c166
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-06-01 21:26:08 +0800

    Update experimental-funcs.lua
    renamed getcallingfunc to getcallingfunction
    getcallingscript now uses a new method
    docs are better?

--- misc/experimental-funcs.lua
+++ misc/experimental-funcs.lua
@@ -1,42 +1,54 @@
 --[[
-	Gets the function that is calling the function that is hooked
-
-	```lua
-	local function test()
-		local funcCaller = getcallingfunc()
-		print(debug.info(lmao, "n")) -- this will print the function name of the caller
-	end
-	test()
-	```
+	TODO:
+	* BUG #1:
+		global funcs that are created in as executor closure should not be returned (it currently doesn't do that)
 --]]
-local function getcallingfunc(indexLvl: number)
-	indexLvl = indexLvl or 3
-	local func = debug.info(indexLvl, "f")
-	return (func or nil)
+
+local function stringPathToInstance(strPath: string)
+	local pathSplit = string.split(strPath, ".")
+	local result = game
+	for _, path in ipairs(pathSplit) do
+		result = (if result then result:FindFirstChild(path) else nil)
+	end
+	return result
 end
 
---[[
-	Gets the script that is calling the function
-
-	```lua
-	local function test()
-		local scriptCaller = getcallingscript()
-		print(scriptCaller.Name) -- this will print the script name of the caller
+--Gets the function that is calling the function that is hooked
+---@param indexLvlStart number
+---@return function
+local function getcallingfunction(indexLvlStart: number)
+	indexLvlStart = indexLvlStart or 10
+	local funcCaller
+	for indexLvl = indexLvlStart, 0, -1 do
+		local func = debug.info(indexLvl, "f")
+		local funcName = (func and debug.info(func, "n") or "")
+		if ((func and not is_fluxus_closure(func)) and (funcName ~= "")) then -- BUG #1
+			funcCaller = func
+			break
+		end
 	end
-	test()
-	```
---]]
-local function getcallingscript(indexLvl: number): LuaSourceContainer
-	indexLvl = indexLvl or 3
-	local func = debug.info(indexLvl, "f")
-	return (func and rawget(getfenv(func), "script") or nil)
+	return funcCaller
 end
 
+--Gets the script that is calling the function
+---@return LocalScript | ModuleScript
+local function getcallingscript()
+	for indexLvl = 10, 0, -1 do
+		local strResult = string.match(debug.traceback("", indexLvl), "%w+:[%d%s]+$")
+		if strResult then
+			local strPath = string.split(strResult, ":")[1]
+			local object = stringPathToInstance(strPath)
+			if object and object:IsA("LuaSourceContainer") then
+				return object
+			end
+		end
+	end
+end
 -- examples:
 
 local function test()
-	local funcCaller, scriptCaller = getcallingfunc(), getcallingscript()
-	print(debug.info(funcCaller, "n"), scriptCaller.Name) -- this will print the function name
+	local funcCaller, scriptCaller = getcallingfunction(), getcallingscript()
+	print(debug.info(funcCaller, "n"), scriptCaller) -- this will print the function name
 end
 
 local function icallthetestfunc()
