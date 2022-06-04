fa73c34b472902f31d83f81ae07c4e43185e9132
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-06-04 18:20:55 +0800

    Update experimental-funcs.lua
    asdasesad

--- misc/experimental-funcs.lua
+++ misc/experimental-funcs.lua
@@ -4,7 +4,6 @@
 		global funcs that are created in as executor closure should not be returned (it currently doesn't do that)
 	
 --]]
-
 --Returns the instance from the string path
 ---@param strPath string
 ---@return Instance | nil
@@ -26,7 +25,7 @@ local function getcallingfunction(indexLvlStart: number)
 	for indexLvl = indexLvlStart, 0, -1 do
 		local func = debug.info(indexLvl, "f")
 		local funcSource = (func and debug.info(func, "s") or nil)
-		if ((func and (stringPathToInstance(funcSource) or not isourclosure(func)))) then -- BUG #1
+		if ((func and (stringPathToInstance(funcSource) or not isourclosure(func)))) then
 			funcCaller = func
 			break
 		end
@@ -35,12 +34,10 @@ local function getcallingfunction(indexLvlStart: number)
 end
 
 --Gets the script that is calling the function
----@return LocalScript | ModuleScript
+---@return LocalScript | ModuleScript | nil
 local function getcallingscript()
 	local funcCaller = getcallingfunction()
-	if funcCaller then
-		return stringPathToInstance(debug.info(funcCaller, "s"))
-	end
+	if funcCaller then return stringPathToInstance(debug.info(funcCaller, "s")) end
 	for indexLvl = 10, 0, -1 do
 		local strResult = string.match(debug.traceback("", indexLvl), "%w+:[%d%s]+$")
 		if strResult then
@@ -51,6 +48,7 @@ local function getcallingscript()
 			end
 		end
 	end
+	return nil
 end
 -- examples:
 
