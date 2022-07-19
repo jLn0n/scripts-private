8e7bc68f0670e0f6ef0c597df56fd1e0d3d9a79c
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-19 22:49:00 +0800

    Update replication-docs.lua

--- misc/replication-docs.lua
+++ misc/replication-docs.lua
@@ -13,3 +13,17 @@ print(getrenv()._G["replication-3088049980[TQPv0hjXcxw%^fX70fuoySxbK]"]) -- key 
 		.getPartOwner(partObj: BasePart): Player
 		.setPartOwner(partObj: BasePart, owner: Player): nil
 --]]
+
+--[[
+	examples: (this creates a part and teleports them after 2.5 seconds)
+	local createObj = replication.instance.new
+	local modifyProperty = replication.instance.modifyProperty
+
+	local newPart = createObj("Part", {
+		Size = Vector3.new(math.random(1, 10), math.random(1, 10), math.random(1, 10)),
+		Parent = workspace
+	})
+	print(newPart)
+
+	task.delay(2.5, modifyProperty(newPart, "Position", (Vector3.yAxis * 69)))
+--]]
