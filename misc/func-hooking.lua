960e017f0efd5747bad105ac18b79b32ddba5cfc
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-19 14:29:42 +0800

    Update and rename func-hooking.lua to replication-docs.lua

--- misc/func-hooking.lua
+++
@@ -1,4 +0,0 @@
--- for this game: https://www.roblox.com/games/10268452785/func-hooking
--- main
-local _ = getrenv()._G["GiveMeAccessToReplicationPls"]
-print(_)
---
+++ misc/replication-docs.lua
@@ -0,0 +1,13 @@
+-- for this game: https://www.roblox.com/games/10268452785
+-- main
+print(getrenv()._G["replication-3088049980[sior@4MHysg%yZGTs8VlwAq8Y]"])
+
+--[[
+	replication:
+	  .instance:
+		.new(className: string, properties: Dictionary): Instance
+		.destroy(object: Instance): nil
+	  .network:
+		.getPartOwner(partObj: BasePart): Player
+		.setPartOwner(partObj: BasePart, owner: Player): nil
+--]]
