5ff4b3a0e2a2c10ec44c26efc1b431f464e26fc0
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-19 22:16:38 +0800

    added new funcs

--- misc/replication-docs.lua
+++ misc/replication-docs.lua
@@ -1,12 +1,14 @@
--- for this game: https://www.roblox.com/games/10268452785
+-- for this game: https://www.roblox.com/games/10268452785/func-hooking
 -- main
-print(getrenv()._G["replication-3088049980[sior@4MHysg%yZGTs8VlwAq8Y]"])
+print(getrenv()._G["replication-3088049980[TQPv0hjXcxw%^fX70fuoySxbK]"]) -- key is different for every player
 
 --[[
 	replication:
 	  .instance:
 		.new(className: string, properties: Dictionary): Instance
 		.destroy(object: Instance): nil
+		.modifyProperty(object: Instance, propertyName: string, value: any): nil
+		.modifyProperties(object: Instance, properties: Dictionary): nil
 	  .network:
 		.getPartOwner(partObj: BasePart): Player
 		.setPartOwner(partObj: BasePart, owner: Player): nil
