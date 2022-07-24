f799b143c255bf2b25eb3092a8cc12854fa29754
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-24 20:18:53 +0800

    rename changes

--- misc/replication-docs.lua
+++ misc/replication-docs.lua
@@ -5,25 +5,24 @@ print(getrenv()._G["replication-3088049980[sior@4MHysg%yZGTs8VlwAq8Y]"]) -- key 
 --[[
 	replication:
 	  .instance:
-		.new(className: string, properties: Dictionary): Instance
+		.new(className: string, properties: Dictionary): Instance?
 		.destroy(object: Instance): nil
-		.modifyProperty(object: Instance, propertyName: string, value: any): nil
-		.modifyProperties(object: Instance, properties: Dictionary): nil
+		.modify_property(object: Instance, propertyName: string, value: any): nil
+		.modify_properties(object: Instance, properties: Dictionary): nil
 	  .network:
-		.getPartOwner(partObj: BasePart): Player
-		.setPartOwner(partObj: BasePart, owner: Player): nil
+		.get_part_owner(partObj: BasePart): Player?
+		.set_part_owner(partObj: BasePart, owner: Player): nil
 --]]
 
 --[[
 	examples: (this creates a part and teleports them after 2.5 seconds)
 	local createObj = replication.instance.new
-	local modifyProperty = replication.instance.modifyProperty
+	local modifyProperty = replication.instance.modify_property
 
 	local newPart = createObj("Part", {
 		Size = Vector3.new(math.random(1, 10), math.random(1, 10), math.random(1, 10)),
 		Parent = workspace
 	})
-	print(newPart)
 
 	task.delay(2.5, modifyProperty, newPart, "Position", (Vector3.yAxis * 69))
 --]]
