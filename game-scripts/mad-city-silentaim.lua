2583c9f51fcc12e7def9464d38f874523ed8057b
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-26 16:43:51 +0800

    Create mad-city-silentaim.lua

---
+++ game-scripts/mad-city-silentaim.lua
@@ -0,0 +1,87 @@
+-- services
+local runService = game:GetService("RunService")
+local players = game:GetService("Players")
+-- objects
+local camera = workspace.CurrentCamera
+local player = players.LocalPlayer
+local mouse = player:GetMouse()
+local fovCircle, targetBox = Drawing.new("Circle"), Drawing.new("Square")
+-- variables
+local oldNamecall, nearestPlr
+local nearPlrs = table.create(0)
+local settings = {
+	visibleCheck = false,
+	fov = 175,
+	knockdownCheck = true
+}
+-- functions
+local function checkPlr(plrArg)
+	local plrHumanoid = plrArg.Character:FindFirstChild("Humanoid")
+	return plrArg ~= player and (plrArg.Neutral or plrArg.TeamColor ~= player.TeamColor) and (plrArg.Character and (plrHumanoid and plrHumanoid.Health ~= 0) and not plrArg.Character:FindFirstChildWhichIsA("ForceField")) and not (settings.knockdownCheck and plrArg.Character:FindFirstChild("Downed") or false)
+end
+local function inLineOfSite(originPos, ...)
+	return #camera:GetPartsObscuringTarget({originPos}, {camera, player.Character, ...}) == 0
+end
+local function getNearestPlrByCursor()
+	table.clear(nearPlrs)
+	for _, plr in ipairs(players:GetPlayers()) do
+		local p_dPart = (plr.Character and plr.Character:FindFirstChild("Head"))
+		if not p_dPart then continue end
+		local posVec3, onScreen = camera:WorldToViewportPoint(p_dPart.Position)
+		local mouseVec2, posVec2 = Vector2.new(mouse.X, mouse.Y), Vector2.new(posVec3.X, posVec3.Y)
+		local fovDist = (posVec2 - mouseVec2).Magnitude
+		if checkPlr(plr) and (not settings.visibleCheck or (onScreen and inLineOfSite(p_dPart.Position, plr.Character))) and (fovDist < settings.fov) then
+			table.insert(nearPlrs, {
+				aimPart = p_dPart,
+				dist = fovDist,
+			})
+		end
+	end
+	table.sort(nearPlrs, function(x, y)
+		return (x.dist < y.dist)
+	end)
+	return (nearPlrs and #nearPlrs ~= 0) and nearPlrs[1] or nil
+end
+-- main
+targetBox.Color = Color3.fromRGB(0, 185, 35)
+targetBox.Filled = true
+targetBox.Size = Vector2.new(20, 20)
+targetBox.Thickness = 20
+targetBox.Transparency = .6
+
+fovCircle.Color = Color3.fromRGB(0, 185, 35)
+fovCircle.Thickness = 2
+fovCircle.Transparency = .6
+fovCircle.Visible = true
+
+runService.Heartbeat:Connect(function()
+	nearestPlr = getNearestPlrByCursor()
+
+	fovCircle.Radius = settings.fov
+	fovCircle.Position = Vector2.new(mouse.X, mouse.Y + 36)
+
+	if nearestPlr then
+		local posVec3, onScreen = camera:WorldToViewportPoint(nearestPlr.aimPart.Position)
+
+		targetBox.Visible = onScreen
+		targetBox.Position = Vector2.new(posVec3.X, posVec3.Y)
+	else
+		targetBox.Visible = false
+		targetBox.Position = Vector3.zero
+	end
+end)
+
+oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+	local args = {...}
+	local namecallMethod = getnamecallmethod()
+
+	if not checkcaller() then
+		if ((namecallMethod == "FireServer" and self.Name == "RemoteEvent") and args[1] == "BulletHit") then
+			if nearestPlr then
+				args[2] = nearestPlr.aimPart.Position
+				args[5] = nearestPlr.aimPart
+			end
+		end
+	end
+	return oldNamecall(self, unpack(args))
+end))
