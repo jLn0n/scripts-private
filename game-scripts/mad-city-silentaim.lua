acefa158e221dc43d164fbb1b9b3ba1841a11f7c
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-27 16:22:46 +0800

    Update mad-city-silentaim.lua

--- game-scripts/mad-city-silentaim.lua
+++ game-scripts/mad-city-silentaim.lua
@@ -28,11 +28,11 @@ local function getNearestPlrByCursor()
 		local p_dPart = (plr.Character and plr.Character:FindFirstChild("Head"))
 		if not p_dPart then continue end
 		local posVec3, onScreen = camera:WorldToViewportPoint(p_dPart.Position)
-		local mouseVec2, posVec2 = Vector2.new(mouse.X, mouse.Y), Vector2.new(posVec3.X, posVec3.Y)
+		local mouseVec2, posVec2 = Vector2.new(mouse.X, mouse.Y + 36), Vector2.new(posVec3.X, posVec3.Y)
 		local fovDist = (mouseVec2 - posVec2).Magnitude
 
 		if checkPlr(plr) and (not settings.visibleCheck or (onScreen and inLineOfSite(p_dPart.Position, plr.Character))) then
-			if (fovDist < settings.fov) and (fovDist < nearestPlrData.dist) then
+			if (fovDist <= settings.fov) and (fovDist < nearestPlrData.dist) then
 				nearestPlrData.aimPart = p_dPart
 				nearestPlrData.dist = fovDist
 			end
