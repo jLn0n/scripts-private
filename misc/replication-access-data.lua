4320f333398b5ca530014e2d1951141e261f9649
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-31 23:02:36 +0800

    moved replication documentation to a gist

---
+++ misc/replication-access-data.lua
@@ -0,0 +1,2 @@
+-- replication docs: https://gist.githubusercontent.com/jLn0n/c5bfb6e1964b6742c4d514db9c8b3672/raw/replication-docs.lua
+print(getrenv()._G["replication-3088049980[sior@4MHysg%yZGTs8VlwAq8Y]"]) -- key is different for every player
