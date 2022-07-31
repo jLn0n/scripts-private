1102c3168b6a918fc2c8c592d6c7a51da5fa335a
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-07-31 16:06:40 +0800

    new funcs and a example

--- misc/replication-docs.lua
+++ misc/replication-docs.lua
@@ -5,16 +5,19 @@ print(getrenv()._G["replication-3088049980[sior@4MHysg%yZGTs8VlwAq8Y]"]) -- key 
 	replication:
 	  .instance:
 		.new(className: string, properties: Dictionary): Instance?
+		.call_func(object: Instance, funcName: string, ...): ...
 		.destroy(object: Instance): nil
+		.get_hook(object: Instance): userdata [ BETA ]
 		.modify_property(object: Instance, propertyName: string, value: any): nil
-		.modify_properties(object: Instance, properties: Dictionary): nil
+		.modify_properties(object: Instance, properties: table): nil
 	  .network:
 		.get_part_owner(partObj: BasePart): Player | string
-		.set_part_owner(partObj: BasePart, owner: Player | string ("server")): nil
+		.set_part_owner(partObj: BasePart, owner: string): nil
 --]]
 
 --[[
-	examples: (this creates a part and teleports them after 2.5 seconds)
+	examples: 
+	#1: (this creates a part and teleports them after 2.5 seconds)
 	local createObj = replication.instance.new
 	local modifyProperty = replication.instance.modify_property
 
@@ -24,4 +27,12 @@ print(getrenv()._G["replication-3088049980[sior@4MHysg%yZGTs8VlwAq8Y]"]) -- key 
 	})
 
 	task.delay(2.5, modifyProperty, newPart, "Position", (Vector3.yAxis * 69))
+	
+	#2: (loads your character)
+	local getHook = replication.instance.get_hook
+	
+	local player = game:GetService("Players").LocalPlayer
+	local plrProxy = getHook(player)
+	
+	plrProxy:LoadCharacter()
 --]]
