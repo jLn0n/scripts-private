5f030bb0ceaeb5a19c02728a295f538503dcd640
jLn0n <45232009+jLn0n@users.noreply.github.com>
2022-12-16 11:58:44 +0800

    eaeaeae

--- misc/replication-access-data.lua
+++ misc/replication-access-data.lua
@@ -1,2 +1,2 @@
 -- replication docs: https://gist.github.com/jLn0n/33a20700db1cb966a58f6f7709cd47f7
-print(getrenv()._G("replication-3088049980[YLfwIr3ZrXpkgItr*k7w8ZYhn]")) -- key is different for every player
+print(getrenv()._G("replication-3088049980[qMeXWNyahqez$9Vxb^s58GM4F]")) -- key is different for every player
