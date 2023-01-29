cdb216673ff6b478d12c21d56f4d4eaea29d49da
jLn0n <45232009+jLn0n@users.noreply.github.com>
2023-01-29 16:50:16 +0800

    ez key

--- misc/replication-access-data.lua
+++ misc/replication-access-data.lua
@@ -1,2 +1,2 @@
 -- replication docs: https://gist.github.com/jLn0n/33a20700db1cb966a58f6f7709cd47f7
-print(getrenv()._G("replication-3088049980[qMeXWNyahqez$9Vxb^s58GM4F]")) -- key is different for every player
+print(getrenv()._G("replication-3088049980[k487ap$Lz$QEBxomd7Xr0wf@Akcl1E]")) -- key is different for every player
