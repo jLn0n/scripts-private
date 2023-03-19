9cd9b641bc0874c2937e202ebcfb224cfe639386
jLn0n <45232009+jLn0n@users.noreply.github.com>
2023-03-19 17:06:43 +0800

    Update anti-fingerprint.lua
    yessir

--- universal-scripts/anti-fingerprint.lua
+++ universal-scripts/anti-fingerprint.lua
@@ -1,17 +1,29 @@
+--[[
+	should be on the autoexec folder of your desired scripting utility,
+	recommended to use autolaunch.
+--]]
+-- settings
+local minOffset, maxOffset =
+	1024,
+	65536
+-- services
+local _localizationService = game:GetService("LocalizationService")
 -- variables
 local randomObj = Random.new()
-local usePlus = randomObj:NextInteger(1, 2) == 2
-local clockOffset, tickOffset = randomObj:NextInteger(256, 32768), randomObj:NextInteger(256, 32768)
+local usePlus = randomObj:NextInteger(1, 10) > 5
+local clockOffset, tickOffset, timestampOffset =
+	randomObj:NextInteger(minOffset, maxOffset),
+	randomObj:NextInteger(minOffset, maxOffset),
+	randomObj:NextInteger(minOffset, maxOffset)
 local fakeLocaleId = string.char(randomObj:NextInteger(97, 122), randomObj:NextInteger(97, 122)) .. "-" .. string.char(randomObj:NextInteger(97, 122), randomObj:NextInteger(97, 122))
+local fakeCountryCode = string.char(randomObj:NextInteger(65, 90), randomObj:NextInteger(65, 90))
 local refs = table.create(0)
-local RandomInfo = {
-	Timezones = {
-		[1] = 'Eastern Daylight Time';
-		[2] = 'Central Daylight Time';
-		[3] = 'Mountain Daylight Time';
-		[4] = 'Pacific Daylight Time';
-	};
- }
+local clockTimezones = {
+	"Eastern Daylight Time",
+	"Central Daylight Time",
+	"Mountain Daylight Time",
+	"Pacific Daylight Time"
+}
 -- functions
 local function offsetNumber(number, offset)
 	return (usePlus and number + offset or number - offset)
@@ -30,23 +42,40 @@ end))
 refs.os_date = hookfunction(os.date, newcclosure(function(...)
 	local format = ...
 
-	if format == "%Z" then
-		return RandomInfo.Timezones[randomObj:NextInteger(1, #RandomInfo.Timezones)]
-	end
-
-	if format == "*t" then
-		local dateTypeResult = refs.os_date(...)
-		dateTypeResult.isdst = randomObj:NextInteger(1, 2) == 2
-		return dateTypeResult
+	if not checkcaller() then
+		if format == "%Z" then
+			return clockTimezones[randomObj:NextInteger(1, #clockTimezones)]
+		end
+		if format == "*t" then
+			local dateTypeResult = refs.os_date(...)
+			dateTypeResult.isdst = randomObj:NextInteger(1, 10) > 5
+			return dateTypeResult
+		end
 	end
 	return refs.os_date(...)
+end))
+
+refs.datetime_now = hookfunction(DateTime.now, newcclosure(function(...)
+	local origDateTimeObj = refs.datetime_now(...)
+	local offsettedDateTimeObj = DateTime.fromUnixTimestampMillis(offsetNumber(origDateTimeObj.UnixTimestampMillis, timestampOffset))
+
+	return (not checkcaller() and offsettedDateTimeObj or origDateTimeObj)
 end))
 
 refs.__index = hookmetamethod(game, "__index", newcclosure(function(self, ...)
 	local args = {...}
 
-	if not checkcaller() and self:IsA("LocalizationService") and args[1] == "SystemLocaleId" then
+	if not checkcaller() and self == _localizationService and (args[1] == "RobloxLocaleId" or args[1] == "SystemLocaleId") then
 		return fakeLocaleId
 	end
 	return refs.__index(self, ...)
+end))
+
+refs.__namecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+	local namecallMethod = getnamecallmethod()
+
+	if not checkcaller() and self == _localizationService and namecallMethod == "GetCountryRegionForPlayerAsync" then
+		return fakeCountryCode
+	end
+	return refs.__namecall(self, ...)
 end))
